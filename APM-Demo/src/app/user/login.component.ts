a5f24cd188cd42d6abb01d91bddb2f23c820be18
Will Gilmour <will.gilmour@runpath.com>
2022-06-23 17:12:36 +0100

    Chapter 6 homework

--- APM-Demo/src/app/user/login.component.ts
+++ APM-Demo/src/app/user/login.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from './auth.service';
+import { getMaskUserName } from './state/user.reducer';
 
 @Component({
   templateUrl: './login.component.html',
@@ -21,9 +22,9 @@ export class LoginComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.store.select('user').subscribe((userState) => {
-      this.maskUserName = userState.maskUserName;
-    });
+    this.store
+      .select(getMaskUserName)
+      .subscribe((maskUserName) => (this.maskUserName = maskUserName));
   }
 
   cancel(): void {
